38da6ae9d09bb4f92730334c2a965f5a213a2d53
ElDiabolotino <rmarqueslpz@gmail.com>
2024-11-15 09:39:35 +0100

    Texte récap des Drivers et class abstraite Driver

---
+++ project_name/Driver.cpp
@@ -0,0 +1,32 @@
+/*********************************************************************
+ * @file  Apllication.cpp
+ * @author "Raphaël Marques" Julie Champagne jfgkfd
+ * @brief Fichier source de l'application
+ *********************************************************************/
+#include "Driver.h"
+
+
+Driver::Driver()
+{
+  // Code
+  ; 
+}
+  
+Driver::~Driver()
+{
+  // Code
+  ;
+}  
+
+void Driver::init(void)
+{
+  // Code
+    ;
+}
+
+
+void Driver::run(void)
+{
+  // Code
+    ;
+}
